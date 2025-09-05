13b6a927e1f234f8d22d8df75211cb56aa61b822
chaitanyautekar20 <chaitanya.utekar20@gmail.com>
2025-09-05 12:23:33 +0530

    gihubdata added

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const githubdata = {
+    "# backendpractice":
+    "basic backend practice"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -18,6 +23,10 @@ app.get('/login',(req,res)=>{
 
 app.get('/youtube',(req,res)=>{
     res.send("<h2>This is the youtube</h2>")
+})
+
+app.get('/githubdata',(req,res)=>{
+    res.json(githubdata)
 })
 
 app.listen(process.env.PORT, () => {
